43c7dfd49975447b6822dd66f85514c396640a1b
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2019-04-12 14:23:21 +0300

    09_gen_server beginning

--- 09_gen_server/exercise/chat_room_manager.erl
+++ 09_gen_server/exercise/chat_room_manager.erl
@@ -3,40 +3,93 @@
 -export([start/0,
          create_room/2, remove_room/2, get_rooms/1,
          add_user/3, remove_user/3, get_users_list/2,
-         send_message/4,  get_messages_history/2]).
+         send_message/4,  get_messages_history/2, stop/1,
+         call/3, loop/1, handle_call/3]).
+
+
+-record(room, {
+    id,
+    name,
+    users = [],
+    messages = []
+}).
 
 
 start() ->
-    ok.
+    InitialState = #{},
+    spawn(?MODULE, loop, [InitialState]).
 
 
 create_room(Server, RoomName) ->
-    ok.
+    call(Server, create_room, RoomName).
 
 
 remove_room(Server, RoomId) ->
-    ok.
+    call(Server, remove_room, RoomId).  
 
 
 get_rooms(Server) ->
-    ok.
+    call(Server, get_rooms, none).
 
 
 add_user(Server, RoomId, UserName) ->
-    ok.
+    call(Server, add_user, {RoomId, UserName}).
 
 
 remove_user(Server, RoomId, UserName) ->
-    ok.
+    call(Server, remove_user, {RoomId, UserName}).
 
 
 get_users_list(Server, RoomId) ->
-    ok.
+    call(Server, get_users_list, RoomId).
 
 
 send_message(Server, RoomId, UserName, Message) ->
-    ok.
+    call(Server, send_message, {RoomId, UserName, Message}).
 
 
 get_messages_history(Server, RoomId) ->
-    ok.
+    call(Server, get_messages_history, RoomId).
+
+
+call(Server, Command, Payload) -> 
+    QueryRef = erlang:monitor(process, Server),
+    Server ! {Command, QueryRef, self(), Payload},
+    receive 
+        {reply, QueryRef, Reply} -> 
+            erlang:demonitor(QueryRef, [flush]),
+            Reply;
+        {'DOWN', QueryRef, _process, _Server, Reason} -> 
+            % io:format("Server crached - Reason: ~p~n", [Reason]),
+            {error, Reason}
+    after 
+        5000 -> 
+            erlang:demonitor(QueryRef, [flush]),
+            {error, no_reply}
+    end.        
+
+
+stop(Server) ->
+    Server ! stop.
+
+
+loop(State) -> 
+    % io:format("V3 Loop ~p Current State: ~p~n", [self(), State]),
+    receive
+        {Command, QueryRef, ClientPid, Payload} ->
+            {Reply, State2} = ?MODULE:handle_call(Command, Payload, State),
+            ClientPid ! {reply, QueryRef, Reply},
+            ?MODULE:loop(State2);
+        stop -> ok;
+        _Msg -> ?MODULE:loop(State)
+    end.
+
+
+handle_call(create_room, RoomName, State) -> ok;
+handle_call(remove_room, RoomId, State) -> ok;
+handle_call(get_rooms, _, State) -> ok;
+handle_call(add_user, {RoomId, UserName}, State) -> ok;
+handle_call(remove_user, {RoomId, UserName}, State) -> ok;
+handle_call(get_users_list, RoomId, State) -> ok;
+handle_call(send_message, {RoomId, UserName, Message}, State) -> ok;
+handle_call(get_messages_history, RoomId, State) -> ok.
