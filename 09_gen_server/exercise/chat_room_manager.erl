1baa4d008d14745b5badbc522b96eb1adcfdbe41
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2019-04-13 11:06:17 +0300

    done 09_gen_server

--- 09_gen_server/exercise/chat_room_manager.erl
+++ 09_gen_server/exercise/chat_room_manager.erl
@@ -7,8 +7,10 @@
          call/3, loop/1, handle_call/3]).
 
 
+-define(CountRooms, 5).
+
+
 -record(room, {
-    id,
     name,
     users = [],
     messages = []
@@ -85,11 +87,75 @@ loop(State) ->
     end.
 
 
-handle_call(create_room, RoomName, State) -> ok;
-handle_call(remove_room, RoomId, State) -> ok;
-handle_call(get_rooms, _, State) -> ok;
-handle_call(add_user, {RoomId, UserName}, State) -> ok;
-handle_call(remove_user, {RoomId, UserName}, State) -> ok;
-handle_call(get_users_list, RoomId, State) -> ok;
-handle_call(send_message, {RoomId, UserName, Message}, State) -> ok;
-handle_call(get_messages_history, RoomId, State) -> ok.
+handle_call(create_room, RoomName, State) -> 
+    case maps:size(State) of 
+        Num when Num >= ?CountRooms -> {{error, room_limit}, State};
+        _Num -> RoomId = make_ref(), Room = #room{name = RoomName},
+            State2 = State#{RoomId => Room},
+                {{ok, RoomId}, State2}
+    end;
+
+
+handle_call(remove_room, RoomId, State) -> 
+    case maps:find(RoomId, State) of 
+        {ok, _Value} -> State2 = maps:remove(RoomId, State), {ok, State2};
+        error -> {{error, room_not_found}, State}
+    end;
+
+% [{RoomId, RoomName}]
+handle_call(get_rooms, _, State) -> 
+    Fun = fun(RoomId, #room{name = RoomName}, Acc) ->
+        [{RoomId, RoomName} | Acc] end,
+    Rooms = maps:fold(Fun, [], State),
+    {Rooms, State};
+
+
+handle_call(add_user, {RoomId, UserName}, State) -> 
+    case maps:find(RoomId, State) of 
+        {ok, #room{users = UserList} = Ulist} -> 
+            case lists:member(UserName, UserList) of 
+                true -> {{error, user_is_in_room}, State};
+                false -> Ulist2 = Ulist#room{users = [UserName | UserList]},
+                         State2 = State#{RoomId => Ulist2}, {ok, State2}         
+            end;
+        error -> {{error, room_not_found}, State}
+    end;
+
+
+handle_call(remove_user, {RoomId, UserName}, State) -> 
+    case maps:find(RoomId, State) of 
+        {ok, #room{users = UserList} = Ulist} -> 
+            case lists:member(UserName, UserList) of 
+                true -> Ulist2 = Ulist#room{users = lists:delete(UserName, UserList)},
+                        State2 = State#{RoomId => Ulist2}, {ok, State2};
+                false -> {{error, user_not_in_room}, State}         
+            end;
+        error -> {{error, room_not_found}, State}
+    end;
+
+
+handle_call(get_users_list, RoomId, State) -> 
+    case maps:find(RoomId, State) of 
+        {ok, #room{users = UserList}} -> 
+            {{ok, UserList}, State};
+        error -> {{error, room_not_found}, State}
+    end;
+
+
+handle_call(send_message, {RoomId, UserName, Message}, State) -> 
+    case maps:find(RoomId, State) of 
+        {ok, #room{users = UserList, messages = Messages} = Ulist} -> 
+            case lists:member(UserName, UserList) of 
+                true -> Ulist2 = Ulist#room{messages = [{UserName, Message} | Messages]}, 
+                        State2 = State#{RoomId => Ulist2}, {ok, State2};
+                false -> {{error, user_not_in_room}, State}        
+            end;
+        error -> {{error, room_not_found}, State}
+    end;
+
+
+handle_call(get_messages_history, RoomId, State) ->
+    case maps:find(RoomId, State) of 
+        {ok, #room{messages = Messages}} -> {{ok, Messages}, State};
+        error -> {{error, room_not_found}, State}
+    end.
