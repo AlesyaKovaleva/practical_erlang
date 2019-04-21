adc9d317c2a4bc5b8fe76791f7886a70cedbb9c4
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2019-04-21 13:40:35 +0300

    done 11_link_monitor

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -1,7 +1,53 @@
 -module(main).
 
--export([parse/1]).
+-export([parse/1, processing/2, merger/2, pidref_to_map/1]).
+
+-define(WORKER, worker).
 
 
 parse(Files) ->
-    ok.
+    PidRef = [{File, spawn_monitor(?WORKER, get_data, [File, self()])} || File <- Files],
+    PidRef2 = pidref_to_map(PidRef),
+    processing(PidRef2, {#{}, #{}}).
+
+
+processing(PidRef, Acc) when map_size(PidRef) == 0 -> Acc;
+processing(PidRef, {DataMap, ErrorMap}) ->
+    receive 
+        {data, Data} -> 
+            DataMap2 = merger(Data, DataMap),
+            processing(PidRef, {DataMap2, ErrorMap});
+
+        {'DOWN', Ref, process, Pid, Reason} -> 
+            PidRef2 = maps:remove({Pid, Ref}, PidRef),
+            case Reason of 
+                normal -> processing(PidRef2, {DataMap, ErrorMap});
+                _ -> File = maps:get({Pid, Ref}, PidRef), 
+                    ErrorMap2 = ErrorMap#{File => Reason},
+                    processing(PidRef2, {DataMap, ErrorMap2})
+            end
+
+    after 2000 -> timeout
+    end.
+
+
+merger(Map1, Map2) -> 
+    maps:fold(
+    fun(Name, Count, Acc) ->
+        case maps:find(Name, Map2) of
+            {ok, Count2} -> Acc#{Name => Count+Count2};
+            error -> Acc#{Name => Count}
+        end
+    end,
+    Map2,
+    Map1).
+
+
+pidref_to_map(PidRef) ->
+    lists:foldl(
+        fun({File, PR}, Acc) ->
+            Acc#{PR => File}
+        end, 
+    #{},
+    PidRef
+    ).
--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,25 @@
 -module(worker).
+
+-export([get_data/2]).
+
+
+get_data(File, PidParent) -> 
+    {ok, Data} = file:read_file(File),
+    Data2 = binary:split(Data, [<<"\n">>], [global]),
+    Data3 = lists:filter(fun(Bin) -> Bin /= <<>> end, Data2),
+    DataMap = lists:foldl(
+        fun(A, Map) -> 
+            List = binary:split(A, [<<",">>], [global]),
+            Key = lists:nth(2, List),
+            Value = list_to_integer(binary_to_list(lists:nth(3, List))),
+            case maps:find(Key, Map) of 
+                {ok, Val} -> Map#{Key := Val+Value};
+                error -> Map#{Key => Value}
+            end
+        end,
+        #{},
+        Data3
+    ),
+
+    PidParent ! {data, DataMap},
+    ok.
