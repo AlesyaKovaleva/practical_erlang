548c1d820847b0730ced29a691b3313a330316a1
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2019-04-24 11:42:19 +0300

    done 13_application

--- 13_application/exercise/ebin/mylib.app
+++ 13_application/exercise/ebin/mylib.app
@@ -1,1 +1,18 @@
-%% you code here
+{
+    application,
+    mylib,
+    [
+        {description, "My Lib"},
+        {vsn, "0.1"},
+        {registered, []},
+        {mod, {mylib_app, []}},
+        {applications, [kernel, stdlib]},
+        {env,[
+            {min_val, 2},
+            {max_val, 10},
+            {connection_timeout, 10000},
+            {query_timeout, 10000}
+        ]},
+        {modules, [mylib_app, mylib_sup, mylib_worker]}
+    ]
+}.
--- 13_application/exercise/src/mylib_worker.erl
+++ 13_application/exercise/src/mylib_worker.erl
@@ -3,3 +3,83 @@
 
 -export([start_link/0, get_version/0, get_modules/0, get_min_val/0, get_connection_timeout/0, all_apps/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [],[]).
+
+
+get_version() -> 
+    gen_server:call(?MODULE, get_version).
+
+
+get_modules() -> 
+    gen_server:call(?MODULE, get_modules).
+
+
+get_min_val() -> 
+    gen_server:call(?MODULE, get_min_val).
+
+
+get_connection_timeout() -> 
+    gen_server:call(?MODULE, get_connection_timeout).
+
+
+all_apps() -> 
+    gen_server:call(?MODULE, all_apps).
+
+
+init([]) -> 
+    {ok, #{}}.
+
+
+handle_call(get_version, _From, State) ->
+    {ok, Reply} = application:get_key(mylib, vsn),
+    {reply, Reply, State};
+
+
+handle_call(get_modules, _From, State) ->
+    {ok, Reply} = application:get_key(mylib, modules),
+    {reply, Reply, State};
+
+
+handle_call(get_min_val, _From, State) ->
+    {ok, Reply} = application:get_env(mylib, min_val),
+    {reply, Reply, State};
+
+
+handle_call(get_connection_timeout, _From, State) ->
+    {ok, Reply} = application:get_env(mylib, connection_timeout),
+    {reply, Reply, State};
+
+
+handle_call(all_apps, _From, State) ->
+    Reply = lists:foldl(
+        fun({Application, Description, Vsn}, Map) ->
+            Map#{Application => #{description => Description, version => Vsn}}
+        end, 
+    #{},
+    application:which_applications()
+    ),
+    
+    {reply, Reply, State};
+
+
+handle_call(_, _From, State) ->
+    {noreply, State}.
+
+
+handle_cast(_, State) ->
+    {noreply, State}.
+
+
+handle_info(_, State) ->
+    {noreply, State}.
+
+
+terminate(_, _) ->
+    ok.
+
+
+code_change(_, State, _) -> 
+    {ok, State}.
